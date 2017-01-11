e48e681aafb9d54bcbd6bd9f0844e2af9dd8b0fa
Brian Murphy-Dye <brian@murphydye.com>
2017-01-11 15:02:46 -0700

    Add feedback file.

---
+++ src/haystack/feedback.clj
@@ -0,0 +1,29 @@
+(ns haystack.feedback
+  (:require [clojure.set :as set]
+            [clojure.string :refer [split join]]
+            [clojure.set :refer [rename-keys]]
+
+            [clojurewerkz.elastisch.rest.index :as esi]
+
+            [haystack.repo :as r]
+            ;; [clj-http.client :as client]
+            ;; [bidi.bidi :as bidi]
+            ))
+
+(defn create-index
+  []
+  (try
+    (do (esi/create r/repo "feedback" {})
+        "created")
+    (catch Throwable e
+      (if (re-find #"already.exists" (str e))
+        "already-existed"
+        (do
+          (println "error in index create")
+          (println (re-find #"already.exists" (str e)))
+          (throw e))))))
+
+(defn get-feedback
+  []
+  (esd/search repo "searchecommerce" "feedback" ""))
+
