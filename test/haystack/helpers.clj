ee23069c04ac2da77f16fcca7bd8322d48848ead
Brian Murphy-Dye <brian@murphydye.com>
2017-01-21 11:42:51 -0700

    Add min-docs and not-in-top test macros.

--- test/haystack/helpers.clj
+++ test/haystack/helpers.clj
@@ -4,26 +4,47 @@
              ))
 
 (defn hit-count
+  "return number of hits from search response"
   [response]
   (-> response :paging :total-items))
 
-(defn extract-matnr
+(defn extract-digits
+  "extract digits (upc/matnr) from string.
+  Useful when possibly inside a span html tag."
   [s]
   (->> s (re-matches #"[^\d]*([\d]+).*") last))
 
 (defn matnrs
+  "return collection of matnrs from search response (lazy)"
   [response]
-  (map #(extract-matnr (:matnr %)) (-> response :documents)))
+  (map #(extract-digits (:matnr %)) (-> response :documents)))
+
+(defmacro min-docs
+  "raise test error if less than n possible documents in search response.
+  This is the number of hits, not the number documents returned in this page."
+  [n]
+  `(is (<= ~n (hit-count ~'*response*))))
 
 (defmacro max-docs
+  "raise test error if more than n possible documents in search response.
+  This is the number of hits, not the number documents returned in this page."
   [n]
   `(is (<= (hit-count ~'*response*) ~n)))
 
+(defmacro not-in-top
+  "raise error if document with given matnr is within the top n results.
+  Use this to ensure a specific document is not scored high."
+  [matnr n]
+  `(is (not (contains? (set (take ~n ~'*matnrs*)) (str ~matnr)))))
+
 (defmacro in-top
+  "raise error if document with given matnr isn't within the top n results.
+  Note: if n>10, should include :num-per-page in search query-map."
   [matnr n]
   `(is (contains? (set (take ~n ~'*matnrs*)) (str ~matnr))))
 
 (defmacro test-search
+  "define search use case"
   [name query-map & tests]
   `(deftest ~name
      (let [~'*response* (search ~query-map)
--- test/haystack/search_test.clj
+++ test/haystack/search_test.clj
@@ -5,24 +5,32 @@
             [haystack.helpers :refer :all]
             ))
 
-(test-search
- matnrs-check
+;; The following are common searches and definitions
+;; of what should be returned.
+
+;; As an organization, we've tended to modify the search
+;; algorithm to improve a specific use case, which often
+;; inadvertenly damaged other common use cases.
+
+;; Defining these search use cases is an attempt
+;; at preventing these perturbations of the force.
+
+(test-search matnrs-check
  {:search "2843977 2542450"}
  (max-docs 50)
  (in-top 2843977 2)
  (in-top 2542450 2)
  )
 
-(test-search
- upcs-check
+(test-search upcs-check
  {:search "078477045442 980100350109"}
  (max-docs 500)
  (in-top 199152 2)
  (in-top 2542450 2)
  )
 
-(test-search
- copper-clip-check
+(test-search copper-clip-check
+ ;; set num-per-page to return up to 100 documents
  {:search "copper clip" :num-per-page 100}
  (max-docs 100)
  (in-top 20127 100)   ;; "copper" and "clip" not in the same field
