2e0b7693ca812d6b9703143e2da7030ccc08c332
Brian Murphy-Dye <brian@murphydye.com>
2017-01-28 08:48:15 -0700

    Add synonyms for 0-9.

--- src/haystack/create_index.clj
+++ src/haystack/create_index.clj
@@ -30,6 +30,17 @@
    "pipe conduit"
    "romex nm"
    "so seoow seow soow sow"
+   "gray grey"
+   "0 zero"
+   "1 one"
+   "2 two"
+   "3 three"
+   "4 four"
+   "5 five"
+   "6 six"
+   "7 seven"
+   "8 eight"
+   "9 nine"
    ])
 
 (defn synonyms
